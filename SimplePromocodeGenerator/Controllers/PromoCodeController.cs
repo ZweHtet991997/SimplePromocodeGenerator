33b4ac15216bb478105b75cec8c2ea5781b7caf5
zwewaiyanhtet99 <zwehtet.nksoftwarehouse@gmail.com>
2025-06-12 14:35:44 +0630

    source code added

---
+++ SimplePromocodeGenerator/Controllers/PromoCodeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimplePromocodeGenerator.Models;
+using System;
+using System.Text;
+
+namespace SimplePromocodeGenerator.Controllers
+{
+    public class PromoCodeController : Controller
+    {
+        [HttpPost("generate")]
+        public IActionResult GeneratePromoCode([FromBody] PromoCodeGenerationRequest request)
+        {
+            var code = GenerateCode(request.CodeLength);
+            var expirationDate = DateTime.Now.AddDays(request.ValidityPeriod);
+            var promoCode = new PromoCode { Code = code, ExpirationDate = expirationDate, DiscountAmount = request.DiscountAmount };
+            return Ok(promoCode);
+        }
+
+        public string GenerateCode(int codeLength)
+        {
+            string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            StringBuilder sb = new StringBuilder();
+            Random random = new Random();
+            while (codeLength-- > 0)
+            {
+                sb.Append(validChars[random.Next(validChars.Length)]);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ SimplePromocodeGenerator/Models/PromoCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimplePromocodeGenerator.Models
+{
+    public class PromoCode
+    {
+        public string Code { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public decimal DiscountAmount { get; set; }
+    }
+
+    public class PromoCodeGenerationRequest
+    {
+        public int CodeLength { get; set; }
+        public int ValidityPeriod { get; set; }
+        public decimal DiscountAmount { get; set; }
+    }
+}
